e27b9cac201f4a2c8f3cd5f11ef483d2bb6d3e0e
Navid Ahrary <navid.ahrary@outlook.com>
2024-08-31 12:08:59 +0330

    Add typescript sample

---
+++ az-204/07-cosmos.ts
@@ -0,0 +1,34 @@
+import { CosmosClient, PartitionKeyKind } from "@azure/cosmos";
+
+const DATABASE_ID = "Certificates";
+const CONTAINER_ID = "Microsoft Azure";
+
+async function main() {
+  // const endpoint = ""
+  const client = new CosmosClient({ endpoint: "", key: "" });
+  const db = await client.databases.createIfNotExists({
+    id: DATABASE_ID,
+    throughput: 400,
+  });
+  const container = await db.database.containers.createIfNotExists({
+    id: CONTAINER_ID,
+    partitionKey: {
+      paths: ["/partitionKey"],
+      kind: PartitionKeyKind.Hash,
+    },
+  });
+  const items = await container.container.items.create({
+    id: "item-01",
+    name: "Azure Fundamental Certificate",
+    cert: "AZ-900",
+  });
+
+  const q = await container.container.items
+    .query({
+      query: "SELECT * FROM Certificates WHERE cert = @cert",
+      parameters: [{ name: "cert", value: "AZ-900" }],
+    })
+    .fetchAll();
+}
+
+main();
