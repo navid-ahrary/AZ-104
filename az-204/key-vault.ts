8be5a0ca81ee7e5d659654bcc1cacf9da3191134
Navid Ahrary <navid.ahrary@outlook.com>
2024-08-31 12:07:42 +0330

    Add typescript sample code

---
+++ az-204/key-vault.ts
@@ -0,0 +1,29 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+import { KeyClient } from "@azure/keyvault-keys";
+
+const KEY_VAULT_URL = "https://kv-frc-demo.vault.azure.net/";
+
+const credential = new DefaultAzureCredential();
+
+async function runSecret() {
+  const client = new SecretClient(KEY_VAULT_URL, credential);
+  const poll = await client.beginRecoverDeletedSecret("key-01");
+  await poll.pollUntilDone();
+  const saved = await client.setSecret("key-01", "navid");
+  console.log(saved);
+
+  const deletePoller = await client.beginDeleteSecret("key-01");
+  await deletePoller.pollUntilDone();
+  await client.purgeDeletedSecret("key-01");
+}
+
+async function runKey() {
+  const client = new KeyClient(KEY_VAULT_URL, credential);
+
+  await client.createRsaKey("MyKey", { keySize: 4096 });
+}
+
+// runSecret();
+
+runKey();
